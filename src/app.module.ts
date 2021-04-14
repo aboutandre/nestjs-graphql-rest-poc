4de7545c674c3a3c1d84e2f5b9ea7a9484e00d7e
Andre Lorch <github@aboutandre.com>
2021-04-14 10:36:38 +0200

    Refactor code from the `main.ts` to `app.module.ts` to fix a pesky bug where the GQL resolvers were not working

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,63 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestModule } from './test/test.module';
-import { join } from 'path';
-import { GraphQLModule } from '@nestjs/graphql';
+import { GraphQLModule, GraphQLSchemaHost } from '@nestjs/graphql';
+import { HttpAdapterHost } from '@nestjs/core';
+import { Application } from 'express';
+import { OpenAPI, useSofa } from 'sofa-api';
+import * as swaggerUi from 'swagger-ui-express';
 
 @Module({
   imports: [
     TestModule,
     GraphQLModule.forRoot({
-      autoSchemaFile: join(process.cwd(), './src/schema.graphql'),
+      autoSchemaFile: true,
     }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  constructor(
+    @Inject(GraphQLSchemaHost) private readonly schemaHost: GraphQLSchemaHost,
+    @Inject(HttpAdapterHost) private readonly httpAdapterHost: HttpAdapterHost,
+  ) {}
+
+  onModuleInit(): void {
+    if (!this.httpAdapterHost) {
+      return;
+    }
+
+    const { httpAdapter } = this.httpAdapterHost;
+    const app: Application = httpAdapter.getInstance();
+    const { schema } = this.schemaHost;
+    const openApi = OpenAPI({
+      schema,
+      info: {
+        title: 'StudyContext REST API',
+        version: '1.0.0',
+      },
+    });
+
+    // convert GraphQL API to REST using SOFA
+    app.use(
+      '/api',
+      useSofa({
+        schema,
+        basePath: '/api',
+        onRoute(info) {
+          openApi.addRoute(info, {
+            basePath: '/api',
+          });
+        },
+      }),
+    );
+
+    // add Swagger page for REST API
+    const openApiDefinitions = openApi.get();
+    openApiDefinitions.paths['/api/add-project-file'] = '/api/add-project-file';
+
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDefinitions));
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { loadSchema } from '@graphql-tools/load';
-import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
-import { OpenAPI, useSofa } from 'sofa-api';
-import * as swaggerUi from 'swagger-ui-express';
-import * as swaggerDocument from './swagger.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const schema = await loadSchema('./src/**/*.graphql', {
-    loaders: [new GraphQLFileLoader()],
-  });
-  const openApi = OpenAPI({
-    schema,
-    info: {
-      title: 'Example API',
-      version: '3.0.0',
-    },
-  });
-  app.use(
-    '/api',
-    useSofa({
-      schema,
-      basePath: '/api',
-      onRoute(info) {
-        openApi.addRoute(info, {
-          basePath: '/api',
-        });
-      },
-    }),
-  );
-
-  openApi.save('./swagger.yml');
-  openApi.save('./swagger.json');
-  app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-  await app.listen(3000);
+  await app.listen(3030);
 }
 
 bootstrap();
